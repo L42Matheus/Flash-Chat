20201a32c4e3ee3b0a161afba17010d779665735
Lucas Matheus <lmatheus@ciandt.com>
2022-08-26 12:39:17 -0300

    Projeto Flash-Chat

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
+        
+        print(db)
+
         return true
     }
 
---
+++ Flash Chat iOS13/model/Message.swift
@@ -0,0 +1,18 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by cit on 24/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    
+    let sender: String
+    let body: String
+    
+    
+}
